0bb9afc863e3ff9a9eff6c019c3ed86178310a99
Vinicius Pacheco Furtado <vin175pacheco@gmail.com>
2018-05-19 16:20:04 -0300

    Set max-age on public/d to 6 months

--- src/server/index.js
+++ src/server/index.js
@@ -17,6 +17,11 @@ const runServer = () => {
   server.use(bodyParser.json())
 
   // Serve public
+  server.use(
+    express.static(path.resolve('public/d'), {
+      maxage: 31104000000, // 6 months
+    }),
+  )
   server.use(express.static(path.resolve('public')))
 
   if (useNext) {
